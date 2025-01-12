c21d74648436f946967c3d13425dfdc887d7412f
Mark Peters <mapeters101@earthlink.net>
2025-01-12 17:49:20 -0500

    Work on lesson005 up to 5:45.

---
+++ lesson005.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")
+print("Hello, " + name + "!")
+
+print("Hello, world!")
+print("The answer is", 42)
+
+print("Name\tAge")
+print("Faisal\n25")
+print("Zamir\t30")
+
+
