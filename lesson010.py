165fe9ccda16ec44d783d980c1ae205975b8edc2
Mark Peters <mapeters101@earthlink.net>
2025-01-14 15:23:38 -0500

    Complete work on lesson010 from 9:50.

--- lesson010.py
+++ lesson010.py
@@ -7,7 +7,7 @@ z = 3+6j # Complex
 is_male = True
 print(is_male)
 
-# set
+# set: non-sequence
 s = {1,2,3,4,5,6}
 print(s)
 
@@ -19,4 +19,16 @@ mydict = {
      "Contact":283289
      }
 print(mydict["name"])
+
+tuple = (1,2,3,4,5) # immutable
+#tuple[0] = 100
+print(tuple) # 0 to 4 : sequence, index
+print(tuple[0])
+print(tuple[4])
+
+lst = [1,2,3,4,5] # mutable
+lst[0] = 100
+print(lst)
+
+
 
