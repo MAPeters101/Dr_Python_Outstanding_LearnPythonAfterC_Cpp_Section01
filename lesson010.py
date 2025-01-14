83a0916dec5b8e303b9688f85b94b2f005e9055f
Mark Peters <mapeters101@earthlink.net>
2025-01-14 14:56:07 -0500

    Work on lesson010 to 9:50.

---
+++ lesson010.py
@@ -0,0 +1,22 @@
+# Numeric Data Types
+x = 10 # int
+y = 20.4 # float
+z = 3+6j # Complex
+
+# Boolean
+is_male = True
+print(is_male)
+
+# set
+s = {1,2,3,4,5,6}
+print(s)
+
+# dictionary
+mydict = {
+     "id":123,
+     "name":"Jafri",
+     "Subject":"Python",
+     "Contact":283289
+     }
+print(mydict["name"])
+
